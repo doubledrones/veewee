a986b77b39f90ecb7b729926c4d0845691777756
Marcin Nowicki <pr0d1r2@gmail.com>
2011-12-19 06:23:07 +0100

    Update to use current stage3 and iso.

--- templates/gentoo-latest-x86_64-experimental/postinstall.sh
+++ templates/gentoo-latest-x86_64-experimental/postinstall.sh
@@ -44,7 +44,7 @@ cd /mnt/gentoo
 
 #Download a stage3 archive
 while true; do
-	wget http://mirror.switch.ch/ftp/mirror/gentoo/releases/amd64/autobuilds/20111013/stage3-amd64-20111013.tar.bz2 && > gotstage3
+	wget http://mirror.switch.ch/ftp/mirror/gentoo/releases/amd64/autobuilds/20111208/stage3-amd64-20111208.tar.bz2 && > gotstage3
         if [ -f "gotstage3" ]
         then
 		break
